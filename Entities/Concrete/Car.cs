1c5a9f5fd4f4e4e3a6f28b0bd274b7d878c1b603
alibyrm <aliosman.bayramtr@gmail.com>
2021-05-17 21:58:13 +0300

    entities eklendi

---
+++ Entities/Concrete/Car.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    //çıplak sınıf kalmasın
+    public class Car: IEntity
+    {
+        public int CarID { get; set; }
+        public int BrandID { get; set; }
+        public int ColorID { get; set; }
+        public string ModelYear { get; set; }
+        public decimal DailyPrice { get; set; }
+        public string Descriptions { get; set; }
+    }
+}
---
+++ Entities/Concrete/Color.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    //çıplak sınıf kalmasın
+    public class Color: IEntity
+    {
+        public int Id { get; set; }
+        public string ColorName { get; set; }
+    }
+}
